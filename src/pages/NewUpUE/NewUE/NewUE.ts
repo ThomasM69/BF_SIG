f97652c5fcf146d76050474c78f785623ff6108b
ThomasM69 <44232628+ThomasM69@users.noreply.github.com>
2020-09-25 16:53:07 +0200

    post ue

--- src/pages/NewUpUE/NewUE/NewUE.ts
+++ src/pages/NewUpUE/NewUE/NewUE.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
     selector: 'page-NewUE',
     templateUrl: 'NewUE.html'
 })
 
+
 export class NewUEPage {
-}
+     ue = {};
+
+    constructor( public http: HttpClient) {
+    }
+
+    saveUeToServer(){
+        this.http.post('http://localhost:5050/ue', this.ue)
+        .subscribe(
+        () => {
+            console.log()
+        });
+    }
+
+}  
