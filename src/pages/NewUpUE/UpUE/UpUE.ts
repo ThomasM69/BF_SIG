4bc6471a145245b06bf0859ce45298b866d70036
ThomasM69 <44232628+ThomasM69@users.noreply.github.com>
2020-10-01 17:35:26 +0200

    Add files via upload

--- src/pages/NewUpUE/UpUE/UpUE.ts
+++ src/pages/NewUpUE/UpUE/UpUE.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
     selector: 'page-UpUE',
     templateUrl: 'UpUE.html'
 })
-
-
 export class UpUEPage {
+    listUE= [];
+
+    constructor( public http: HttpClient) {
+    }
+
+    getUeList(){
+        this.http.get('http://localhost:5050/ue/').subscribe((response:any) => {
+            console.log(response);
+            this.listUE = response;
+        });
+    }
+
+
 }
