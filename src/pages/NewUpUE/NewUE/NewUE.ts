55427885d792d52a31551563a5a38cdb188737f3
ThomasM69 <44232628+ThomasM69@users.noreply.github.com>
2020-10-01 17:36:52 +0200

    Add files via upload

--- src/pages/NewUpUE/NewUE/NewUE.ts
+++ src/pages/NewUpUE/NewUE/NewUE.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+// import  {Observable} from 'rxjs/Observable'
 
 
 @Component({
@@ -8,13 +8,18 @@ import { HttpClient } from '@angular/common/http';
     templateUrl: 'NewUE.html'
 })
 
-
 export class NewUEPage {
-     ue = {};
+
+    ue= {};
+
+    //TEST2
+    // result:any= [];
+    // data: Observable<any>;
 
     constructor( public http: HttpClient) {
     }
 
+    // TEST1
     saveUeToServer(){
         this.http.post('http://localhost:5050/ue', this.ue)
         .subscribe(
@@ -23,4 +28,14 @@ export class NewUEPage {
         });
     }
 
-}  
+    //TEST2(suite)
+    // postData(){
+    //     let postData = new FormData();
+    //     postData.append('id','classe' );
+        
+    //    this.data = this.http.post('http://localhost:5050/ue/', postData)
+    //     this.data.subscribe
+    //     (data => { console.log(data);
+    //     });
+    // }
+}
