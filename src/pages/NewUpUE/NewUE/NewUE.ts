c01e9fafe1ef9285b864386137aad14748987304
ThomasM69 <44232628+ThomasM69@users.noreply.github.com>
2020-10-12 11:12:48 +0200

    Add files via upload

--- src/pages/NewUpUE/NewUE/NewUE.ts
+++ src/pages/NewUpUE/NewUE/NewUE.ts
@@ -12,27 +12,11 @@ export class NewUEPage {
 
     ue= {};
 
-    //TEST2
-    // result:any= [];
-    // data: Observable<any>;
-
     constructor( public http: HttpClient) {
     }
 
-    // TEST1
     saveUeToServer(){
         this.http.post('http://localhost:5050/ue', this.ue, {headers: new HttpHeaders({'Content-Type':'application/json', Accept : 'application/json', Authorization:''})})
-        .subscribe((response) => { console.log(response) });
+        .subscribe((response) => { console.log(response )});
     }
-
-    //TEST2(suite)
-    // postData(){
-    //     let postData = new FormData();
-    //     postData.append('id','classe' );
-        
-    //    this.data = this.http.post('http://localhost:5050/ue/', postData)
-    //     this.data.subscribe
-    //     (data => { console.log(data);
-    //     });
-    // }
 }
